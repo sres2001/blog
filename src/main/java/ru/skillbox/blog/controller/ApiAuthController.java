bb2c8091cdf50415829422dcd6632c1ffd56dcc2
sres2001 <G1t[eE38.5}>
2021-06-16 17:49:10 +0700

    stage 1: controller mappings

--- src/main/java/ru/skillbox/blog/controller/ApiAuthController.java
+++ src/main/java/ru/skillbox/blog/controller/ApiAuthController.java
@@ -1,8 +1,10 @@
 package ru.skillbox.blog.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/auth")
 public class ApiAuthController {
 
 }
