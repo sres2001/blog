04a6d769de4d28d33e932ee18caecaf47abd03a1
sres2001 <G1t[eE38.5}>
2021-06-25 11:39:14 +0700

    For not found pages we redirect user to index page.

--- src/main/java/ru/skillbox/blog/controller/DefaultController.java
+++ src/main/java/ru/skillbox/blog/controller/DefaultController.java
@@ -1,14 +1,34 @@
 package ru.skillbox.blog.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
-public class DefaultController {
+public class DefaultController implements ErrorController {
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("error")
+    public String error(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            if (Integer.parseInt(status.toString()) == HttpStatus.NOT_FOUND.value()) {
+                return "index";
+            }
+        }
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
     }
 }
